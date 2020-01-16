aba8b4933cc9390d599f8bd6b694143ac11c3db1
kuang <393427042@qq.com>
2020-01-16 17:04:32 +0800

    压缩文件操作

--- src/main/java/org/r/tool/importimage/service/ScriptService.java
+++ src/main/java/org/r/tool/importimage/service/ScriptService.java
@@ -8,8 +8,7 @@ import org.springframework.util.StreamUtils;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.util.Enumeration;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
+import java.util.zip.*;
 
 @Service
 @Slf4j
@@ -47,6 +46,24 @@ public class ScriptService {
         }
     }
 
+    /**
+     *  压缩文件夹
+     * @param dirPath 文件夹路径
+     * @param descDir 目标文件路径
+     */
+    public void compressZip(String dirPath, String descDir) {
+        try {
+            compress(dirPath, descDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 解压压缩文件
+     * @param zipPath 压缩文件路径
+     * @param descDir 目标目录
+     */
     public void decompressZip(String zipPath, String descDir) {
         File zipFile = new File(zipPath);
         File pathFile = new File(descDir);
@@ -86,6 +103,76 @@ public class ScriptService {
             zip.close();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    static final int BUFFER = 8192;
+
+    private void compress(String srcPath , String dstPath) throws IOException{
+        File srcFile = new File(srcPath);
+        File dstFile = new File(dstPath);
+        if (!srcFile.exists()) {
+            throw new FileNotFoundException(srcPath + "不存在！");
+        }
+
+        FileOutputStream out = null;
+        ZipOutputStream zipOut = null;
+        try {
+            out = new FileOutputStream(dstFile);
+            CheckedOutputStream cos = new CheckedOutputStream(out,new CRC32());
+            zipOut = new ZipOutputStream(cos);
+            String baseDir = "";
+            compress(srcFile, zipOut, baseDir);
+        }
+        finally {
+            if(null != zipOut){
+                zipOut.close();
+                out = null;
+            }
+
+            if(null != out){
+                out.close();
+            }
+        }
+    }
+
+    private void compress(File file, ZipOutputStream zipOut, String baseDir) throws IOException{
+        if (file.isDirectory()) {
+            compressDirectory(file, zipOut, baseDir);
+        } else {
+            compressFile(file, zipOut, baseDir);
+        }
+    }
+
+    /** 压缩一个目录 */
+    private void compressDirectory(File dir, ZipOutputStream zipOut, String baseDir) throws IOException{
+        File[] files = dir.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            compress(files[i], zipOut, baseDir + dir.getName() + "/");
+        }
+    }
+
+    /** 压缩一个文件 */
+    private void compressFile(File file, ZipOutputStream zipOut, String baseDir)  throws IOException{
+        if (!file.exists()){
+            return;
+        }
+
+        BufferedInputStream bis = null;
+        try {
+            bis = new BufferedInputStream(new FileInputStream(file));
+            ZipEntry entry = new ZipEntry(baseDir + file.getName());
+            zipOut.putNextEntry(entry);
+            int count;
+            byte data[] = new byte[BUFFER];
+            while ((count = bis.read(data, 0, BUFFER)) != -1) {
+                zipOut.write(data, 0, count);
+            }
+
+        }finally {
+            if(null != bis){
+                bis.close();
+            }
         }
     }
 }
